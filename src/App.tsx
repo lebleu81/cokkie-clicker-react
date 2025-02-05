1d83c0c2173565a04390109a6f02609b5d233c26
Your lebleu81 <filsdorian@gmail.com>
2025-02-05 14:50:15 +0100

    last modif

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
 
-<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+const App = () => {
 
-const App = ()=> {
+  const [cookies, setCookies] = useState(0)
+
 
   return (
-    <>
-      <h1>Hello, World</h1>
-    </>
+    <div className="w-screen h-screen flex">
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <p>Total cookies : {cookies}</p>
+        <img 
+          onClick={() => { setCookies(cookies + 1)}} 
+          className="h-40" 
+          src="https://cdn-icons-png.flaticon.com/512/216/216341.png" 
+          alt="cookie" />
+      </div>
+      <div className="flex-1 bg-red-100">items</div>
+      <div className="flex-1 bg-blue-100">shop</div>
+    </div>
   )
 }
 
